d9d437af635f5d4a4f3c7da25b588fe39d10044d
Daniel Nguyen <dnguy303@calpoly.edu>
2022-03-10 15:01:23 -0800

    Update tinyFsDemo.py

--- tinyFsDemo.py
+++ tinyFsDemo.py
@@ -13,15 +13,17 @@ run_test('Make FS', tfs_mkfs, 'test_backing_store.bin', 10240)  # make fs
 # mount fs
 run_test('Mount FS', tfs_mount, "BACKING_STORE.bin")
 # open fs
-run_test('Open FS', tfs_openFile, "test.txt")
+FDTest = run_test('Open FS', tfs_openFile, "test.txt")
 # create file
-
 # write file
 # read byte
+run_test('Read Byte', tfs_readByte, FDTest)
 # read whole file
 # seek
 # read byte
+run_test('Read Byte', tfs_readByte, FDTest)
 # close file
+run_test('Close File', tfs_closeFile, FDTest)
 # open file
 FDTest1 = run_test('Open FS', tfs_openFile, "test1.txt")
 # read file
@@ -43,6 +45,18 @@ run_test("Read Directory", tfs_readdir)
 run_test("Delete File", tfs_deleteFile(FDTest1))
 # list files
 run_test("Read Directory", tfs_readdir)
+# open file
+FDTest3 = run_test('Open FS', tfs_openFile, "test3.txt")
+# read fd test 3 for timestamp check
+# write fd test 3 for timestamp check
+# display timestamps
+run_test('timestamp creation', tfs_stat_creation, FDTest2)
+run_test('timestamp creation', tfs_stat_read, FDTest2)
+run_test('timestamp creation', tfs_stat_write, FDTest2)
+
+run_test('timestamp creation', tfs_stat_creation, FDTest3)
+run_test('timestamp creation', tfs_stat_read, FDTest3)
+run_test('timestamp creation', tfs_stat_write, FDTest3)
 # unmount fs
 run_test("Unmount FS", tfs_unmount)
 
